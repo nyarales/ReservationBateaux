6036f38e6b06ae82e1ca19b8011b68714225e356
Ibrahim-elyazidi <ibrahim.el-yazidi@lyceedecachan-sti.fr>
2022-12-08 10:55:19 +0100

    Modification Pages

--- Symfony/Header.php
+++ Symfony/Header.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php"><img src="../Images/Liberty.png" alt="LibertyLines_Logo" weight = 50 height = "50"> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
